de3bcfa7174218cc373f29b57aa7a5a6116362bc
Dylan Reid <dgreid@chromium.org>
2021-04-02 14:12:16 -0700

    ergodox: more mac friendly

--- keyboards/ergodox_ez/keymaps/dgreid/keymap.c
+++ keyboards/ergodox_ez/keymaps/dgreid/keymap.c
@@ -13,12 +13,14 @@ extern keymap_config_t keymap_config;
 #define _SYMBOL 1
 #define _NUMPAD 2
 #define _FUNCTION 3
+#define _SPECIAL 4
 
 enum custom_keycodes {
   DVORAK = SAFE_RANGE,
   SYMBOL,
   NUMPAD,
   FUNCTION,
+  SPECIAL,
   RAISE,
   LOWER,
   ADJUST,
@@ -27,39 +29,40 @@ enum custom_keycodes {
 #define SYMB MO(_SYMBOL)  // Momentary toggle layer SYMBOL.
 #define NUMP MO(_NUMPAD)  // Momentary toggle layer NUMPAD.
 #define LAY_FUNC MO(_FUNCTION)  // Momentary toggle layer FUNCTION.
+#define LAY_SPECIAL MO(_SPECIAL)  // Momentary toggle layer SPECIAL.
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DVORAK] = LAYOUT_ergodox(
       // left hand
-      KC_DEL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_LGUI,
-      KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,   LAY_FUNC,
+      KC_DEL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   LAY_FUNC,
+      KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,   LAY_SPECIAL,
       KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,
       KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,   NUMP,
-      KC_LCTL, KC_LALT, KC_LGUI, KC_LGUI, KC_LCTL,
+      KC_LCTL, KC_LALT, KC_LGUI, KC_LALT, KC_LCTL,
 
       // left thumb
                       //--------------+-----------//
                       //              |           //
-                              KC_LGUI,   KC_CAPS, //
+                              KC_LALT,   KC_CAPS, //
                       //              |           //
       //--------------+---------------+-----------//
       //              |               |           //
                                          KC_INS,  //
       //              |               |           //
-            SYMB,             NUMP,   //----------//
+            SYMB,             KC_LGUI,//----------//
       //              |               |           //
-                                         KC_LALT, //
+                                         NUMP,    //
       //              |               |           //
       //              |               |           //
       //--------------+---------------+-----------//
 
       // right hand
-      KC_RGUI,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-      LAY_FUNC, KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_TILD,
-                KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_ENT,
-      KC_ESC,   KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_SLSH,
-                         KC_RSFT, KC_RALT, KC_RGUI, KC_SLSH, KC_RCTL,
+      LAY_FUNC,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+      LAY_SPECIAL,  KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_TILD,
+                    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_ENT,
+      NUMP,         KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_SLSH,
+                             KC_RSFT, KC_RALT, KC_RGUI, KC_SLSH, KC_RCTL,
 
       // right thumb
       //-----------------+----------------//
@@ -82,21 +85,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_DEL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_LGUI,
       KC_TAB,  KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_Y,   KC_LALT,
       KC_ESC,  KC_LT,   KC_GT,   KC_LPRN, KC_RPRN, KC_DQT,
-      KC_LSFT, KC_SCLN, KC_Q,    KC_TILD, KC_DLR,  KC_DLR,   KC_LCTL,
+      KC_LSFT, KC_SCLN, KC_Q,    KC_TILD, KC_DLR,  KC_DLR,   NUMP,
       KC_LCTL, KC_LALT, KC_LGUI, KC_LALT, KC_LCTL,
 
       // left thumb
                       //--------------+-----------//
                       //              |           //
-                              KC_LGUI,   KC_CAPS, //
+                              KC_LALT,   KC_CAPS, //
                       //              |           //
       //--------------+---------------+-----------//
       //              |               |           //
                                          KC_INS,  //
       //              |               |           //
-            SYMB,             NUMP,   //----------//
+            SYMB,             KC_LGUI,//----------//
       //              |               |           //
-                                         KC_LALT, //
+                                         NUMP,    //
       //              |               |           //
       //              |               |           //
       //--------------+---------------+-----------//
@@ -135,15 +138,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       // left thumb
                       //--------------+-----------//
                       //              |           //
-                              KC_LGUI,   KC_CAPS, //
+                              KC_LALT,   KC_CAPS, //
                       //              |           //
       //--------------+---------------+-----------//
       //              |               |           //
                                          KC_INS,  //
       //              |               |           //
-            SYMB,             NUMP,   //----------//
+            SYMB,             KC_LGUI,//----------//
       //              |               |           //
-                                         KC_LALT, //
+                                         NUMP,    //
       //              |               |           //
       //              |               |           //
       //--------------+---------------+-----------//
@@ -181,13 +184,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       // left thumb
                       //--------------+-----------//
                       //              |           //
-                              NUMP,      KC_CAPS, //
+                              KC_LALT,   KC_CAPS, //
                       //              |           //
       //--------------+---------------+-----------//
       //              |               |           //
                                          KC_INS,  //
       //              |               |           //
-            SYMB,             NUMP,   //----------//
+            SYMB,             KC_LGUI,//----------//
       //              |               |           //
                                          KC_LALT, //
       //              |               |           //
@@ -200,6 +203,52 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_COMM, KC_F4,   KC_F5,   KC_F6,   KC_F11,  KC_ENT,
       KC_ESC,   KC_DOT,  KC_F1,   KC_F2,   KC_F3,   KC_F10,  KC_SLSH,
                          KC_RSFT, KC_RALT, KC_RGUI, KC_SLSH, KC_RCTL,
+
+      // right thumb
+      //-----------------+----------------//
+      //                 |                //
+           KC_HOME,            KC_RGUI,   //
+      //                 |                //
+      //-----------------+-----------------+-----------//
+      //                 |                 |           //
+           KC_END,
+      //                 |                 |           //
+      //-----------------|                 |           //
+      //                 |                 |           //
+          KC_RALT,             KC_BSPC,        KC_SPC  //
+      //                 |                 |           //
+      //-----------------+-----------------+-----------//
+  ),
+  [_SPECIAL] = LAYOUT_ergodox(
+      // left hand
+      KC_DEL,  KC_1,    KC_2,          KC_3,          KC_4,          KC_5,    KC_LGUI,
+      KC_TAB,  KC_PGUP, KC_PGUP,       LCTL(KC_UP),   KC_PGDN,       KC_HOME, LAY_FUNC,
+      KC_ESC,  KC_PGDN, LCTL(KC_LEFT), LCTL(KC_DOWN), LCTL(KC_RGHT), KC_END,
+      KC_LSFT, KC_SCLN, KC_Q,          KC_J,          KC_K,          KC_X,    NUMP,
+      KC_LCTL, KC_LALT, KC_LGUI,       KC_LALT,       KC_LCTL,
+
+      // left thumb
+                      //--------------+-----------//
+                      //              |           //
+                              KC_LALT,   KC_CAPS, //
+                      //              |           //
+      //--------------+---------------+-----------//
+      //              |               |           //
+                                         KC_INS,  //
+      //              |               |           //
+            SYMB,             KC_LGUI,//----------//
+      //              |               |           //
+                                         KC_LALT, //
+      //              |               |           //
+      //              |               |           //
+      //--------------+---------------+-----------//
+
+      // right hand
+      KC_RGUI,  KC_6,    KC_F10,             KC_F11,               KC_F12,  KC_0,    KC_BSPC,
+      LAY_FUNC, KC_0,    KC_MPRV,            KC_MPLY,              KC_MNXT, KC_VOLU,  KC_TILD,
+                KC_COMM, KC_F4,              KC_F5,                KC_F6,   KC_VOLD,  KC_ENT,
+      KC_ESC,   KC_DOT,  KC_BRIGHTNESS_UP,   KC_BRIGHTNESS_DOWN,   KC_F3,   KC_MUTE,  KC_SLSH,
+                         KC_RSFT,            KC_RALT,              KC_RGUI, KC_SLSH, KC_RCTL,
 
       // right thumb
       //-----------------+----------------//
