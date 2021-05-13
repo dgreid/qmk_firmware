cd6dcbd7aa6179baf7c73c139810fb206540efc0
Dylan Reid <dgreid@chromium.org>
2021-05-13 15:31:40 -0700

    tab on right thumb

--- keyboards/ergodox_ez/keymaps/dgreid/keymap.c
+++ keyboards/ergodox_ez/keymaps/dgreid/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       //              |               |           //
             SYMB,             KC_LGUI,//----------//
       //              |               |           //
-                                         NUMP,    //
+                                         KC_LALT, //
       //              |               |           //
       //              |               |           //
       //--------------+---------------+-----------//
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       //                 |                 |           //
       //-----------------|                 |           //
       //                 |                 |           //
-          KC_RALT,             KC_BSPC,        KC_SPC  //
+          KC_TAB,              KC_BSPC,        KC_SPC  //
       //                 |                 |           //
       //-----------------+-----------------+-----------//
 
