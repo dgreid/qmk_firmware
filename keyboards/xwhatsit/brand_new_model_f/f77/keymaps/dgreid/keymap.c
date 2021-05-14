b9c93b43c8437bb601356f934aff5d3b456e1261
Dylan Reid <dgreid@chromium.org>
2021-05-13 21:48:37 -0700

    right command added

--- keyboards/xwhatsit/brand_new_model_f/f77/keymaps/dgreid/keymap.c
+++ keyboards/xwhatsit/brand_new_model_f/f77/keymaps/dgreid/keymap.c
@@ -35,14 +35,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,    KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,        KC_INS,    KC_HOME, KC_PGUP,
         KC_ESC,     KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,     KC_DEL,    KC_END,  KC_PGDN,
         KC_LSFT, KC_NUBS,KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_RSFT,   MO(_FN),   KC_UP,   RESET, // TODO(remove reset)
-        KC_LCTRL, KC_LALT, KC_LGUI,                      KC_SPC,           KC_NO, KC_RALT, MO(_FN), KC_RCTRL,    KC_LEFT,   KC_DOWN, KC_RIGHT
+        KC_LCTRL, KC_LALT, KC_LGUI,                      KC_SPC,           KC_NO, KC_RGUI, KC_RALT,  KC_RCTRL,   KC_LEFT,   KC_DOWN, KC_RIGHT
     ),
     [_FN] = LAYOUT_all(
         KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8,   KC_F9, KC_F10,    KC_F11, KC_F12, KC_INS, KC_DEL,           KC_P7,   KC_P8,   KC_P9,
         KC_NO,      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP, KC_NO, KC_DEL,                 KC_P4,   KC_P5,   KC_P6,
         _______,     KC_VOLD, KC_VOLU, KC_MUTE, KC_EJCT, KC_NO, KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT, KC_RIGHT, KC_NO, KC_NO,  KC_P1,   KC_P2,   KC_P3,
-        _______, _______, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PPLS, KC_PMNS, KC_END, KC_PGDN, KC_DOWN, _______, _______,            KC_P0,   KC_UP,   KC_PDOT,
-        _______, KC_NO, _______,                             MO(_DEBUG),                 _______,  _______, KC_NO, _______,           KC_LEFT, KC_DOWN, KC_RIGHT
+        _______, _______, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PPLS, KC_PMNS, KC_END, KC_PGDN, KC_DOWN, _______, _______,            _______, KC_UP,   KC_PDOT,
+        _______, KC_NO, _______,                             MO(_DEBUG),                 _______,  _______, _______, _______,         KC_LEFT, KC_DOWN, KC_RIGHT
     ),
     [_DEBUG] = LAYOUT_all(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______,
